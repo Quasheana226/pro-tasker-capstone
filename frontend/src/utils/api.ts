4a8d5187c1af4b1628705e86340370b40c0d4f90
Quasheana <quasheana@gmail.com>
2026-06-02 11:28:16 -0400

    add axios instance with automatic JWT auth header injection

---
+++ frontend/src/utils/api.ts
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+// Base URL points to our backend
+const api = axios.create({
+  baseURL: 'http://localhost:5000/api',
+});
+
+// Automatically attach JWT token to every request
+api.interceptors.request.use((config) => {
+  const user = localStorage.getItem('user');
+  if (user) {
+    const { token } = JSON.parse(user);
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
